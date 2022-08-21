746326f0be3123843853d4616c38ef1f335f5922
Matt <mjd014@live.co.uk>
2022-08-21 20:05:29 +0000

    written our first test for the addition function from calc.js file.

--- scripts/tests/cal.test.js
+++ scripts/tests/cal.test.js
@@ -1,9 +1,14 @@
 const addition = require("../calc");
 
+// Calculator test, Addition. Expected results are within the toBe method.
+// Refer to the calc.js for the 'addition' function. 
 describe("Calculator", () => {
     describe("Addition function", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
         });
     });
     describe("Subtract function", () => {
